eeeae0f8a823e54945589bb054eff6fbb8eaab10
ttoddo <hramchencov@gmail.com>
2025-02-03 19:01:36 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Мы запустились
+      </h2>
+      <input></input>
+    </div>
+  );
+}
+
+export default App;
